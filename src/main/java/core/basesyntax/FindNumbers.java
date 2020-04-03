1db706e1b57727e295ec833cf81adf405c669c46
6ALLIKA <skorik009@gmail.com>
2020-04-03 13:27:24 +0300

    added solution

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -11,6 +11,13 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        String[] onlyNumbers = text.replaceAll("[^0-9\\-]", " ").trim().split(" +");
+        int[] result = new int[onlyNumbers.length];
+        int i = 0;
+        for (String number : onlyNumbers) {
+            result[i] = Integer.parseInt(number) * 2;
+            i++;
+        }
+        return result;
     }
 }
