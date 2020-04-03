3478c4f5109529ee148b4c131740917f427419f7
NastiaFasova <fasovaaa@fmail.com>
2020-04-03 14:54:11 +0300

    Added solution to the 1-st task of HW-6

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -2,15 +2,12 @@ package core.basesyntax;
 
 public class FindNumbers {
 
-    /**
-     * <p>Реализуйте метод getAllNumbers(String text) который принимает строку, cодержащую буквы,
-     * целые числа и иные символы.</p>
-     * <p>Требуется: все числа, которые встречаются в строке, поместить в отдельный целочисленный
-     * массив, каждый элемент этого массива умножить на 2. Метод должен возвращать этот массив.</p>
-     * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
-     * числа 92, 18, 26 и 0.</p>
-     */
     public int[] getAllNumbers(String text) {
-        return null;
+        String[] textArr = text.replaceAll("[^0-9\\-]"," ").trim().split("\\s+");
+        int[] numArr = new int[textArr.length];
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = Integer.parseInt(textArr[i]) * 2;
+        }
+        return numArr;
     }
 }
