4ad8ba0e40537ad68857c1e87ed35be57aed0575
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-03 15:47:31 +0300

    hw_6_1_new

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -15,7 +15,7 @@ public class FindNumbers {
     public int[] getAllNumbers(String text) {
         String[] arrayWords = text.replaceAll("[^0-9\\-]", " ").split("[^0-9\\-]");
         return Arrays.stream(arrayWords)
-                .filter(x->x.length()>0)
+                .filter(x -> x.length() > 0)
                 .mapToInt(s -> (Integer.parseInt(s) * 2))
                 .toArray();
     }
