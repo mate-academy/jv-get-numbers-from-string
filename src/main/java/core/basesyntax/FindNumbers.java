ee2b314f9c7f66cce0bbb0d64fdee89a7e151dd9
ana-dav <opa.super.op@gmail.com>
2020-04-05 14:07:00 +0300

    6.1

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,16 +1,20 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class FindNumbers {
 
-    /**
-     * <p>Реализуйте метод getAllNumbers(String text) который принимает строку, cодержащую буквы,
-     * целые числа и иные символы.</p>
-     * <p>Требуется: все числа, которые встречаются в строке, поместить в отдельный целочисленный
-     * массив, каждый элемент этого массива умножить на 2. Метод должен возвращать этот массив.</p>
-     * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
-     * числа 92, 18, 26 и 0.</p>
-     */
     public int[] getAllNumbers(String text) {
-        return null;
+        String str = text.replaceAll("[^0-9]", " ").replaceAll("\\s+"," ");
+
+        int[] result = Arrays.stream(str.split(""))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] *= result[i];
+        }
+
+        return result;
     }
 }
