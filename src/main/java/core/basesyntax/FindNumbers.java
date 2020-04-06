94fa41a9bc311530b9b20fbf2ed518b1fbe159b3
Andrey Tanchak <tanchak12@gmail.com>
2020-04-06 12:23:04 +0300

    added solution2

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,25 +1,12 @@
 package core.basesyntax;
 
 public class FindNumbers {
-
     public int[] getAllNumbers(String text) {
-        String a = "";
-
-        for (int i = 0; i < text.length() - 1; i++) {
-            if (Character.isDigit(text.charAt(i)) || (text.charAt(i) == '-')) {
-                a = a + text.charAt(i);
-                if (!Character.isDigit(text.charAt(i + 1))) {
-                    a = a + " ";
-                }
-            }
-        }
-        if ((Character.isDigit(text.charAt(text.length() - 1)))) {
-            a = a + text.charAt(text.length() - 1);
-        }
-        String[] mass = a.split(" ");
-        int[] result = new int[mass.length];
-        for (int i = 0; i < mass.length; i++) {
-            result[i] = Integer.parseInt(mass[i]) * 2;
+        String[] maxText = text.replaceAll("([^0-9|-])+"," ").trim()
+                .split(" ");
+        int[] result = new int[maxText.length];
+        for (int i = 0; i < maxText.length; i++) {
+            result[i] = Integer.parseInt(maxText[i]) * 2;
         }
         return result;
     }
