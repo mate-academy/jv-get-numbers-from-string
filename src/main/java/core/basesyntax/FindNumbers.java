ff6aec21664fb1735d64c513196410713b0331e6
My <vasylnv19@gmail.com>
2020-04-06 13:13:25 +0300

    added change by names

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class FindNumbers {
     public int[] getAllNumbers(String text) {
-        String [] nubers = text.replaceAll("[^\\d\\-]+", " ")
+        String [] numbers = text.replaceAll("[^\\d\\-]+", " ")
                 .trim().split(" ");
-        int[] result = new int[nubers.length];
-        for (int i = 0; i < nubers.length; i++) {
-            result[i] = Integer.parseInt(nubers[i]) * 2;
+        int[] result = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = Integer.parseInt(numbers[i]) * 2;
         }
         return result;
     }
