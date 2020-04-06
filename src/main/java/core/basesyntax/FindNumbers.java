58f9791d61ef5fe0280f18a49e04631c22050e3d
nastya <cctvporsh@gmail.com>
2020-04-06 13:55:12 +0300

    Changed regex and removed an if-statement

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -15,11 +15,9 @@ public class FindNumbers {
      */
     public int[] getAllNumbers(String text) {
         List<Integer> list = new ArrayList<Integer>();
-        String[] stringArray = text.replaceAll("[^-0-9]", " ").trim().split(" ");
+        String[] stringArray = text.replaceAll("[^-0-9]+", " ").trim().split(" ");
         for (String s : stringArray) {
-            if (!s.equals("")) {
-                list.add(Integer.parseInt(s) * 2);
-            }
+            list.add(Integer.parseInt(s) * 2);
         }
         int[] result = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
