2d2f44101142eff17a5cae49e8fa33504808463d
petrukillya <62096295+petrukillya@users.noreply.github.com>
2020-04-06 09:59:53 +0300

    fixed code style issue
    Co-Authored-By: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -11,7 +11,7 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        String[] temp = text.replaceAll("[^-0-9]+"," ").trim().split(" ");
+        String[] temp = text.replaceAll("[^-0-9]+", " ").trim().split(" ");
         int[] numbers = new int[temp.length];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = Integer.parseInt(temp[i]) * 2;
