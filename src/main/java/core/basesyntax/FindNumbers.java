70ae8ede3fb693846e7f8426eb032dc213e5f654
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-06 12:53:24 +0300

    Reduced replaceAll calls to 2, because I need to make first replace for spaces and second for trimming leading space

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -14,8 +14,7 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     int[] getAllNumbers(String text) {
-        String s = text.replaceAll("[^0-9*?:.+\\-^\"_ ]", " ").replaceAll(" +\\s", " ");
-        s = s.replaceAll("^\\s*", "");
+        String s = text.replaceAll("[^0-9*?:.+-]+", " ").replaceAll("^[ ]", "");
         String[] words = s.split(" ");
         List<Integer> list = new ArrayList<>();
         for (String word : words) {
