c9e0c3131b1dec4cec40b2b567a7c523133823dc
Leonid <rommel@ukr.net>
2020-08-02 20:22:08 +0300

    task done

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.stream.Stream;
+
 public class FindNumbers {
 
     /**
@@ -11,6 +13,10 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        return Stream.of(text.split("[^-?0-9]+"))
+                .filter(string -> !string.isEmpty())
+                .mapToInt(Integer::valueOf)
+                .map(i -> i * 2)
+                .toArray();
     }
 }
