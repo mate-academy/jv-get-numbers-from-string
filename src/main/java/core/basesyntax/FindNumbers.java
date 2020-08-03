3cf6377704dfb33b9099f8d9676d0f4d4f3e9f12
TimoxaV <vadim2493@gmail.com>
2020-08-03 13:46:01 +0300

    added solution 6.1

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+
 public class FindNumbers {
 
     /**
@@ -11,6 +13,17 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        String[] resultString = text.replaceAll("[^0-9-]", ":").split(":");
+        ArrayList<Integer> numberList = new ArrayList<>();
+        for (int i = 0; i < resultString.length; i++) {
+            if (resultString[i].length() > 0) {
+                numberList.add(Integer.parseInt(resultString[i]) * 2);
+            }
+        }
+        int[] resultArray = new int[numberList.size()];
+        for (int i = 0; i < resultArray.length; i++) {
+            resultArray[i] = numberList.get(i);
+        }
+        return resultArray;
     }
 }
