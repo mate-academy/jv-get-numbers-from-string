15e3d5b261bd1dad66efcdb7291ff861e568239d
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-03 23:39:18 +0300

    Now with greedy modifier! It`s shorter

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -2,20 +2,10 @@ package core.basesyntax;
 
 public class FindNumbers {
     public int[] getAllNumbers(String text) {
-        String[] numbers = text.replaceAll("[^0-9+-]"," ").trim().split(" ");
-        int quantity = 0;
-        for (String number:numbers) {
-            if(number.length()>0){
-                quantity++;
-            }
-        }
-        int[] answer = new int[quantity];
-        quantity = 0;
-        for (String number:numbers) {
-            if(number.length()>0){
-                answer[quantity] = 2*Integer.parseInt(number);
-                quantity++;
-            }
+        String[] numbers = text.replaceAll("[^0-9+-]", " ").trim().split("\\s+");
+        int[] answer = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = 2 * Integer.parseInt(numbers[i]);
         }
         return answer;
     }
