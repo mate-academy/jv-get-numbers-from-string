7d131329cfec4fbd4fad39288d63c4cad6018871
m.mikhajlov <lizappy31@gmail.com>
2020-08-03 19:28:17 +0300

    homework 6-1

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -8,9 +8,16 @@ public class FindNumbers {
      * <p>Требуется: все числа, которые встречаются в строке, поместить в отдельный целочисленный
      * массив, каждый элемент этого массива умножить на 2. Метод должен возвращать этот массив.</p>
      * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
-     * числа 92, 18, 26 и 0.</p>
+     * числа 96, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        String digits = text.replaceAll("[^0-9|^\\-]", " ")
+                .replaceAll("\\s+", " ");
+        String[] digitsArray = digits.trim().split(" ");
+        int[] result = new int[digitsArray.length];
+        for (int i = 0; i < digitsArray.length; i++) {
+            result[i] = Integer.parseInt(digitsArray[i]) * 2;
+        }
+        return result;
     }
 }
