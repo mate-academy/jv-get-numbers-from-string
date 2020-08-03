ba55f5882a1bb4752087892d0c84b7fb43277b4e
Roman <matviichuk.office@gmail.com>
2020-08-03 12:10:38 +0300

    added solution

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class FindNumbers {
 
     /**
@@ -11,6 +15,15 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        ArrayList<String> removeEmptyBox = new ArrayList<>();
+        Collections.addAll(removeEmptyBox, text.replaceAll("[^-\\d]", " ").split(" "));
+        removeEmptyBox.removeAll(Collections.singletonList(""));
+        int[] result = new int[removeEmptyBox.size()];
+        int index = 0;
+        for (String value : removeEmptyBox) {
+            result[index] = Integer.parseInt(value) * 2;
+            index++;
+        }
+        return result;
     }
 }
