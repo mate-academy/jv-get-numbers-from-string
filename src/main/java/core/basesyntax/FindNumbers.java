6623634049c0766d2241eb64f20e74732751f639
Viktor Nikolaev <nikolaev.vikt@gmail.com>
2020-08-03 13:49:38 +0300

    [ADD] solution hw-6.1

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class FindNumbers {
 
     /**
@@ -10,7 +12,14 @@ public class FindNumbers {
      * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
      * числа 92, 18, 26 и 0.</p>
      */
-    public int[] getAllNumbers(String text) {
-        return null;
+    public static int[] getAllNumbers(String text) {
+        String[] tmp = text.replaceAll("[^0-9-]", " ").trim().split("\\s+");
+        int[] result = new int[tmp.length];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.parseInt(tmp[i]) * 2;
+        }
+
+        return result;
     }
 }
