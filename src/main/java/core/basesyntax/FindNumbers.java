32c84f30fac1341ee6c71b002edd3d380e2f3eb5
FYarkiy <flx_jb@yahoo.com>
2020-08-03 14:45:02 +0300

    Double each int from String

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -11,6 +11,11 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        String[] tableString = (text.replaceAll("[^-?\\d+]", " ").trim().split(" +"));
+        int[] tableInt = new int[tableString.length];
+        for (int i = 0; i < tableInt.length; i++) {
+            tableInt[i] = (int) Integer.parseInt(tableString[i]) * 2;
+        }
+        return tableInt;
     }
 }
