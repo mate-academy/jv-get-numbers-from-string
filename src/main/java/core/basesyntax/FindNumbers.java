2a95bd2d78313c44f3e7322943d016956ff07f87
FYarkiy <flx_jb@yahoo.com>
2020-08-04 13:09:11 +0300

    change regex from +] to ]+

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -11,7 +11,7 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        String[] tableString = (text.replaceAll("[^-?\\d+]", " ").trim().split(" +"));
+        String[] tableString = (text.replaceAll("[^-?\\d]+", " ").trim().split(" "));
         int[] tableInt = new int[tableString.length];
         for (int i = 0; i < tableInt.length; i++) {
             tableInt[i] = Integer.parseInt(tableString[i]) * 2;
