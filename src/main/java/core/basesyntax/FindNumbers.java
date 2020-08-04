074943791af97829714d8106a79ef85ef36ed6b9
ROMASHSX <andriiromash@gmail.com>
2020-08-04 08:13:18 +0300

    unusable check was wiped

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -11,16 +11,12 @@ public class FindNumbers {
      * числа 92, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        if (text.equals("")) {
-            return null;
-        } else {
-            text = text.replaceAll("[^0-9-]", " ").trim();
-            String[] splitNumbers = text.split("\\s+");
-            int[] result = new int[splitNumbers.length];
-            for (int i = 0; i < splitNumbers.length; i++) {
-                result[i] = Integer.parseInt(splitNumbers[i]) * 2;
-            }
-            return result;
+        text = text.replaceAll("[^0-9-]", " ").trim();
+        String[] splitNumbers = text.split("\\s+");
+        int[] result = new int[splitNumbers.length];
+        for (int i = 0; i < splitNumbers.length; i++) {
+            result[i] = Integer.parseInt(splitNumbers[i]) * 2;
         }
+        return result;
     }
 }
