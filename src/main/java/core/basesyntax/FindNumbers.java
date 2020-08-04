098de61355d586b5a1e3c64217a42b070fb220a0
Dubovaya <dubovaya.java@gmail.com>
2020-08-04 11:59:29 +0300

    fix6.1

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -6,7 +6,6 @@ public class FindNumbers {
 
     public int[] getAllNumbers(String text) {
         text = text.replaceAll("[^(-?\\d)]", " ").trim();
-
         return Arrays.stream(text.split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .map(i -> i * 2)
